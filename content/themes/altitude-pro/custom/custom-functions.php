8940ebac47d061de2fa5c414309f08b8e828435b
Russell Fair <rfair404@gmail.com>
2016-06-22 13:38:19 -0400

    update styles again

--- content/themes/altitude-pro/custom/custom-functions.php
+++ content/themes/altitude-pro/custom/custom-functions.php
@@ -2,7 +2,7 @@
 
 //register our custom stylesheet, to be called last of course
 function fsa_register_styles() {
-    wp_register_style( 'fsa-custom', get_stylesheet_directory_uri() . '/custom/css/main.css', array('altitude-pro-theme'), '001', 'screen');
+    wp_register_style( 'fsa-custom', get_stylesheet_directory_uri() . '/custom/css/main.css', array('altitude-pro-theme'), '002', 'screen');
 }
 add_action( 'wp_enqueue_scripts', 'fsa_register_styles' );
 
